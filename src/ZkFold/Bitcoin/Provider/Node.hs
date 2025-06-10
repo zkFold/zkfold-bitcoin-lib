49863496185e0bdb6bc2cb559084a97f145d54ae
sourabhxyz <lambdaproofs@pm.me>
2025-06-10 13:00:13 +0530

    feat(#3): add remaning functions bitcoin node provider

--- src/ZkFold/Bitcoin/Provider/Node.hs
+++ src/ZkFold/Bitcoin/Provider/Node.hs
@@ -1,10 +1,16 @@
-module ZkFold.Bitcoin.Provider.Node () where
+module ZkFold.Bitcoin.Provider.Node (
+  nodeBestBlockHash,
+  module ZkFold.Bitcoin.Provider.Node.ApiEnv,
+) where
 
 import Control.Monad ((<=<))
-import Data.Aeson (Value)
+import Data.Aeson (ToJSON (..), Value)
+import Data.Aeson qualified as Aeson
 import Data.ByteString.Char8 qualified as BS8
 import Data.Proxy (Proxy (..))
 import Data.Text (Text)
+import GHC.IsList (IsList (..))
+import GHC.Natural (Natural)
 import Network.HTTP.Client qualified as HttpClient
 import Network.HTTP.Client.TLS qualified as HttpClientTLS
 import Servant.API (
@@ -26,7 +32,7 @@ import Servant.Client (
   runClientM,
  )
 import Servant.Client qualified as Servant
-import ZkFold.Bitcoin.Provider.Node.ApiEnv (NodeApiEnv, handleNodeError, runNodeClient)
+import ZkFold.Bitcoin.Provider.Node.ApiEnv
 import ZkFold.Bitcoin.Provider.Node.Class (ToJSONRPC (..))
 import ZkFold.Bitcoin.Provider.Node.Request (NodeRequest (..))
 import ZkFold.Bitcoin.Provider.Node.Response (NodeResponse (..))
@@ -37,12 +43,34 @@ instance ToJSONRPC GetBestBlockHash where
   toMethod = const "getbestblockhash"
   toParams = const Nothing
 
+data GetBlockHeader = GetBlockHeader Text
+
+instance ToJSONRPC GetBlockHeader where
+  toMethod = const "getblockheader"
+  toParams (GetBlockHeader blockHash) = Just (Aeson.Array $ fromList [toJSON blockHash, Aeson.Bool False])
+
+data GetBlockHash = GetBlockHash Natural
+
+instance ToJSONRPC GetBlockHash where
+  toMethod = const "getblockhash"
+  toParams (GetBlockHash height) = Just (Aeson.Array $ fromList [toJSON height])
+
 type NodeApi =
   ReqBody '[JSON] (NodeRequest GetBestBlockHash) :> Post '[JSON] (NodeResponse Text)
+    :<|> ReqBody '[JSON] (NodeRequest GetBlockHeader) :> Post '[JSON] (NodeResponse Text)
+    :<|> ReqBody '[JSON] (NodeRequest GetBlockHash) :> Post '[JSON] (NodeResponse Text)
 
-bestBlockHash :: NodeRequest GetBestBlockHash -> ClientM (NodeResponse Text)
-bestBlockHash = client @NodeApi Proxy
+-- bestBlockHash :: NodeRequest GetBestBlockHash -> ClientM (NodeResponse Text)
+bestBlockHash :<|> blockHeader :<|> blockHash = client @NodeApi Proxy
 
 nodeBestBlockHash :: NodeApiEnv -> IO Text
 nodeBestBlockHash env =
   handleNodeError "nodeBestBlockHash" <=< runNodeClient env $ bestBlockHash (NodeRequest GetBestBlockHash)
+
+nodeBlockHeader :: NodeApiEnv -> Text -> IO Text
+nodeBlockHeader env givenBlockHash =
+  handleNodeError "nodeBlockHeader" <=< runNodeClient env $ blockHeader (NodeRequest (GetBlockHeader givenBlockHash))
+
+nodeBlockHash :: NodeApiEnv -> Natural -> IO Text
+nodeBlockHash env givenHeight =
+  handleNodeError "nodeBlockHash" <=< runNodeClient env $ blockHash (NodeRequest (GetBlockHash givenHeight))
