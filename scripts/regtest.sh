4c2acd5f0300134dc28ad665bed7a24a065093d5
sourabhxyz <lambdaproofs@pm.me>
2025-07-23 15:48:16 +0530

    feat(#17): e2e regtest

--- scripts/regtest.sh
+++ scripts/regtest.sh
@@ -2,7 +2,7 @@ export datadir=$(mktemp -d "/tmp/bitcoind-regtest-XXXXXX")
 echo "Data directory (datadir) for storage of regtest files: $datadir"
 bitcoind -regtest -datadir=$datadir -fallbackfee=0.0001000 -txindex=1 -daemon -rpcuser=user -rpcpassword=password
 
-btc () {
+btc() {
     bitcoin-cli -regtest -datadir=$datadir -rpcport=18443 -rpcuser=user -rpcpassword=password "$@"
 }
 
@@ -16,12 +16,12 @@ sleep 5
 
 
 PRIVKEY="tprv8ZgxMBicQKsPd66qSfNTYkdM76NsJ368nHs7r1WnKhmUbdx4Gwkhk175pvpe2A652Xzszhg2qf55w8qpRzNBwMboA3R6PoABT36eHV89dRZ"
-btc createwallet "testwallet" false true
+btc createwallet "testwallet" false true > /dev/null
 ORIGINAL_DESCRIPTOR="wpkh($PRIVKEY/*)"
 DESCRIPTOR_INFO=$(btc getdescriptorinfo "$ORIGINAL_DESCRIPTOR")
 CHECKSUM=$(echo $DESCRIPTOR_INFO | jq -r '.checksum')
 FULL_DESCRIPTOR="$ORIGINAL_DESCRIPTOR#$CHECKSUM"
-btc -rpcwallet=testwallet importdescriptors "[{\"desc\": \"$FULL_DESCRIPTOR\", \"timestamp\": \"now\", \"active\": true, \"internal\": false, \"range\": [0,0]}]"
+btc -rpcwallet=testwallet importdescriptors "[{\"desc\": \"$FULL_DESCRIPTOR\", \"timestamp\": \"now\", \"active\": true, \"internal\": false, \"range\": [0,0]}]" > /dev/null
 # Address should be bcrt1qcdu42ejr7nyraa93p8x303a0gnvqz8xq9lx639
 ADDRESS=$(btc -rpcwallet=testwallet getnewaddress "" "bech32")
 echo "Address: $ADDRESS (derived from known extended privkey $PRIVKEY)"
@@ -30,7 +30,13 @@ btc generatetoaddress 1 "$ADDRESS" > /dev/null
 echo "Funded"
 
 echo "Adding dummy blocks by sending funds to a dummy address such that coinbase bitcoin of our test wallet reach maturity"
-btc generatetoaddress 100 "bcrt1qs758ursh4q9z627kt3pp5yysm78ddny6txaqgw" > /dev/null
+
+mineDummyBlocks() {
+  btc generatetoaddress $1 "bcrt1qs758ursh4q9z627kt3pp5yysm78ddny6txaqgw" > /dev/null
+}
+
+mineDummyBlocks 100
+
 
 cleanup() {
     echo "Stopping bitcoind"
@@ -43,4 +49,8 @@ cleanup() {
 trap "cleanup; exit 0" INT TERM
 
 echo "RegTest is running. Press Ctrl+C to stop."
-while true; do sleep 60; done
+
+while true; do
+  mineDummyBlocks 1
+  sleep 10
+done
--- src/ZkFold/Bitcoin/Test/Constants.hs
+++ src/ZkFold/Bitcoin/Test/Constants.hs
@@ -1,11 +1,17 @@
 module ZkFold.Bitcoin.Test.Constants (
   testWalletAddress,
+  testWalletAddress2,
   testWalletXPrvKey,
+  testWalletXPrvKey2,
   testWalletXPubKey,
+  testWalletXPubKey2,
   testWalletMasterXPrvKey,
   testWalletMasterXPubKey,
+  testWalletMasterXPubKey2,
   exportedTestWalletMasterXPrvKey,
+  exportedTestWalletMasterXPrvKey2,
   exportedTestWalletMasterXPubKey,
+  exportedTestWalletMasterXPubKey2,
 ) where
 
 import Data.Function ((&))
@@ -15,33 +21,66 @@ import Haskoin (Address, Base58, Mnemonic, Seed, XPrvKey, XPubKey, btcRegTest, d
 mnemonic :: Mnemonic
 mnemonic = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo vote"
 
+mnemonic2 :: Mnemonic
+mnemonic2 = "hamster diagram private dutch cause delay private meat slide toddler razor book happy fancy gospel tennis maple dilemma loan word shrug inflict delay length"
+
 mnemonicSeed :: Seed
 mnemonicSeed = mnemonicToSeed "" mnemonic & either error id
+
+mnemonicSeed2 :: Seed
+mnemonicSeed2 = mnemonicToSeed "" mnemonic2 & either error id
 
 testWalletMasterXPrvKey :: XPrvKey
 testWalletMasterXPrvKey = makeXPrvKey mnemonicSeed
 
+testWalletMasterXPrvKey2 :: XPrvKey
+testWalletMasterXPrvKey2 = makeXPrvKey mnemonicSeed2
+
 exportedTestWalletMasterXPrvKey :: Base58
 exportedTestWalletMasterXPrvKey = testWalletMasterXPrvKey & xPrvExport btcRegTest
+
+exportedTestWalletMasterXPrvKey2 :: Base58
+exportedTestWalletMasterXPrvKey2 = testWalletMasterXPrvKey2 & xPrvExport btcRegTest
 
 {-# NOINLINE testWalletMasterXPubKey #-}
 testWalletMasterXPubKey :: XPubKey
 testWalletMasterXPubKey = unsafePerformIO $ withContext $ \ctx -> pure $ deriveXPubKey ctx testWalletMasterXPrvKey
 
+{-# NOINLINE testWalletMasterXPubKey2 #-}
+testWalletMasterXPubKey2 :: XPubKey
+testWalletMasterXPubKey2 = unsafePerformIO $ withContext $ \ctx -> pure $ deriveXPubKey ctx testWalletMasterXPrvKey2
+
 {-# NOINLINE exportedTestWalletMasterXPubKey #-}
 exportedTestWalletMasterXPubKey :: Base58
 exportedTestWalletMasterXPubKey = unsafePerformIO $ withContext $ \ctx -> pure $ xPubExport btcRegTest ctx testWalletMasterXPubKey
+
+{-# NOINLINE exportedTestWalletMasterXPubKey2 #-}
+exportedTestWalletMasterXPubKey2 :: Base58
+exportedTestWalletMasterXPubKey2 = unsafePerformIO $ withContext $ \ctx -> pure $ xPubExport btcRegTest ctx testWalletMasterXPubKey2
 
 {-# NOINLINE testWalletXPrvKey #-}
 testWalletXPrvKey :: XPrvKey
 testWalletXPrvKey = unsafePerformIO $ withContext $ \ctx -> pure $ prvSubKey ctx testWalletMasterXPrvKey 0
 
+{-# NOINLINE testWalletXPrvKey2 #-}
+testWalletXPrvKey2 :: XPrvKey
+testWalletXPrvKey2 = unsafePerformIO $ withContext $ \ctx -> pure $ prvSubKey ctx testWalletMasterXPrvKey2 0
+
 {-# NOINLINE testWalletXPubKey #-}
 testWalletXPubKey :: XPubKey
 testWalletXPubKey = unsafePerformIO $ withContext $ \ctx -> pure $ pubSubKey ctx testWalletMasterXPubKey 0
+
+{-# NOINLINE testWalletXPubKey2 #-}
+testWalletXPubKey2 :: XPubKey
+testWalletXPubKey2 = unsafePerformIO $ withContext $ \ctx -> pure $ pubSubKey ctx testWalletMasterXPubKey2 0
 
 {-# NOINLINE testWalletAddress #-}
 testWalletAddress :: Address
 testWalletAddress = unsafePerformIO $ withContext $ \ctx -> pure $ xPubWitnessAddr ctx $ pubSubKey ctx testWalletMasterXPubKey 0
 
+{-# NOINLINE testWalletAddress2 #-}
+testWalletAddress2 :: Address
+testWalletAddress2 = unsafePerformIO $ withContext $ \ctx -> pure $ xPubWitnessAddr ctx $ pubSubKey ctx testWalletMasterXPubKey2 0
+
 -- TODO: Get rid of unsafePerformIO and expose something like mkSetup.
+-- TODO: Likely create a setup with multiple test wallets (under something like list)
